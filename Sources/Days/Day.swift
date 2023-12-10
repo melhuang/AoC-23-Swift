2f09bbeaf5006d1f5a5292cbaa2c4bb4c25839c7
Melissa Huang <melissa.huang@melissa.huang-pro>
2023-12-09 19:35:03 -0800

    some more starter

--- Sources/Days/Day.swift
+++ Sources/Days/Day.swift
@@ -11,3 +11,17 @@ protocol Day {
     func partOne()
     func partTwo()
 }
+
+//fileprivate let input: [String] = FileLoader.load(file: "Day_10")!
+
+//struct Day10: Day {
+//    func partOne() {
+//        var total = 0
+//        print("answer1 is: \(total)")
+//    }
+//    
+//    func partTwo() {
+//        var total = 0
+//        print("answer2 is: \(total)")
+//    }
+//}
---
+++ Sources/Days/Day10.swift
@@ -0,0 +1,23 @@
+//
+//  Day10.swift
+//
+//
+//  Created by Melissa Huang on 12/9/23.
+//
+
+import Foundation
+
+fileprivate let input: [String] = FileLoader.load(file: "Day_10")!
+
+
+struct Day10: Day {
+    func partOne() {
+        var total = 0
+        print("answer1 is: \(total)")
+    }
+    
+    func partTwo() {
+        var total = 0
+        print("answer2 is: \(total)")
+    }
+}
